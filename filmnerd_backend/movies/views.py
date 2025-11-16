212960baea42b710d3b06b88ded499fd8dcab03b
FBeni-04 <benjaminferenczi@gmail.com>
2025-11-16 11:53:47 +0100

    refactor(review/backend): text field could be null

--- filmnerd_backend/movies/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Movie
-from .serializers import MovieSerializer
-
-class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all().order_by("title")
-    serializer_class = MovieSerializer
-    lookup_field = "movie_id"
