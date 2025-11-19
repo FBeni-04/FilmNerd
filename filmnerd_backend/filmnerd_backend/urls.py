761e96b036f33b6c78871e0d2016aa176d4549eb
FBeni-04 <benjaminferenczi@gmail.com>
2025-11-19 14:08:13 +0100

    add default url

--- filmnerd_backend/filmnerd_backend/urls.py
+++ filmnerd_backend/filmnerd_backend/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path("", health),
     path("api/", include("reviews.urls")),
 ]
